796165d1930b978f32c7bd08f54cde55404e83b4
mattRicotta <mattRicotta@fakeemail.com>
2022-10-27 17:50:43 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Magic Boa
+---
+  
+Project for a custom LED-powered faux fur boa.
---
+++ adafruit_whip.ino
@@ -0,0 +1,186 @@
+//Code by Erin St. Blaine for Adafruit.com
+
+#include <FastLED.h>
+
+#define LED_PIN    1
+#define COLOR_ORDER GRB
+#define NUM_LEDS 50
+
+int HUE = 0;
+int SATURATION = 255;
+int BRIGHTNESS = 255;
+int STEPS = 50;  
+
+const byte BUTTON_PIN  =  0;
+uint8_t gHue = 0; // rotating "base color" used by confetti
+
+CRGB leds[NUM_LEDS];
+TBlendType    currentBlending;
+CRGBPalette16 currentPalette;
+
+//BUTTON SETUP STUFF
+byte prevKeyState = HIGH;         // button is active low
+
+unsigned long keyPrevMillis = 0;
+const unsigned long keySampleIntervalMs = 25;
+byte longKeyPressCountMax = 80;    // 80 * 25 = 2000 ms
+byte longKeyPressCount = 0;
+
+int ledMode = 1;  //FIRST ACTIVE MODE
+
+//ADDITIONAL COLOR PALETTES
+const TProgmemPalette16 Murica_p PROGMEM =
+{
+    CRGB::Red,
+    CRGB::Red, // 'white' is too bright compared to red and blue
+    CRGB::Blue,
+    CRGB::Black,
+    
+    CRGB::Red,
+    CRGB::Gray,
+    CRGB::Blue,
+    CRGB::Black,
+    
+    CRGB::Red,
+    CRGB::Red,
+    CRGB::Gray,
+    CRGB::Gray,
+    CRGB::Blue,
+    CRGB::Blue,
+    CRGB::Black,
+    CRGB::Black
+};
+
+//------------------SETUP------------------
+void setup()  
+{
+  delay( 3000 ); // power-up safety delay
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  FastLED.addLeds<WS2812B, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness(  BRIGHTNESS );
+  currentBlending = LINEARBLEND;
+ 
+}
+  
+#define NUM_MODES 6
+//------------------MAIN LOOP------------------
+void loop() {
+    switch (ledMode) {
+       case 999: break;
+       case 0: currentPalette = PartyColors_p; rainbow(); break;
+       case 1: currentPalette = OceanColors_p; rainbow(); break;                    
+       case 2: currentPalette = LavaColors_p; rainbow(); break;   
+       case 3: currentPalette = ForestColors_p; rainbow(); break;
+       case 4: currentPalette = Rainbow_gp; rainbow(); break;
+       case 5: currentPalette = Murica_p; rainbow(); break;
+       case 6: currentPalette = RainbowStripeColors_p; rainbow(); break;
+       case 888: ledMode=0; break;
+       } 
+       
+        // key management section
+    if (millis() - keyPrevMillis >= keySampleIntervalMs) {
+        keyPrevMillis = millis();
+        
+        byte currKeyState = digitalRead(BUTTON_PIN);
+        
+        if ((prevKeyState == HIGH) && (currKeyState == LOW)) {
+            keyPress();
+        }
+        else if ((prevKeyState == LOW) && (currKeyState == HIGH)) {
+            keyRelease();
+        }
+        else if (currKeyState == LOW) {
+            longKeyPressCount++;
+        }
+        
+        prevKeyState = currKeyState;
+    }
+       
+   EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow    
+   FastLED.show(); 
+   FastLED.delay(20); 
+
+}     
+
+
+//BUTTON CONTROL STUFF
+// called when button is kept pressed for less than 2 seconds
+void shortKeyPress() {
+    //Serial.println("short");
+    ledMode++;
+    if (ledMode > NUM_MODES){
+    ledMode=0; }
+}
+
+
+// called when button is kept pressed for more than 2 seconds
+void longKeyPress() {
+    //Serial.println("long");
+    ledMode=888;
+}
+
+
+// called when key goes from not pressed to pressed
+void keyPress() {
+    //Serial.println("key press");
+    longKeyPressCount = 0;
+}
+
+
+// called when key goes from pressed to not pressed
+void keyRelease() {
+    //Serial.println("key release");
+    if (longKeyPressCount >= longKeyPressCountMax) {
+        longKeyPress();
+    }
+    else {
+        shortKeyPress();
+    }
+
+
+    // other code goes here
+}
+
+     
+// SOLID ----------------------------------------------------
+void solid()
+{
+ 
+  fill_solid(leds, NUM_LEDS, CHSV( HUE, SATURATION, BRIGHTNESS));     
+  FastLED.show();
+   
+   }
+   
+
+void rainbow()
+{
+  
+  static uint8_t startIndex = 0;
+  startIndex = startIndex + 1; /* motion speed */
+
+  FillLEDsFromPaletteColors( startIndex);
+
+  FastLED.show();
+  FastLED.delay(20);
+  
+  }
+
+//this bit is in every palette mode, needs to be in there just once
+void FillLEDsFromPaletteColors( uint8_t colorIndex)
+{ 
+  for( int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = ColorFromPalette( currentPalette, colorIndex, BRIGHTNESS, currentBlending);
+    colorIndex += STEPS;
+  }
+}
+
+
+
+void confetti() 
+{
+  // random colored speckles that blink in and fade smoothly
+  fadeToBlackBy( leds, NUM_LEDS, 10);
+  int pos = random16(NUM_LEDS);
+  leds[pos] += CHSV( gHue + random8(64), 200, 255);
+    FastLED.show(); 
+}
